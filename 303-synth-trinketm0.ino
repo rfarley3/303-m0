d39185f079422af116a7804294206ea3e1ba3f4b
R Farley <rfarley3@users.noreply.github.com>
2023-09-18 01:07:41 -0400

    Fix distortion/pops/static, sounds good, fixed bitwidth to and from updateAudio to avoid overflows, like from LPF, very SAMD21 specific

--- 303-synth-trinketm0.ino
+++ 303-synth-trinketm0.ino
@@ -12,6 +12,7 @@
     https://diyelectromusic.wordpress.com/2021/02/15/midi-in-for-3-3v-microcontrollers/
     https://diyelectromusic.wordpress.com/2021/05/29/midi-connections-cheat-sheet/
     https://github.com/diyelectromusic/sdemp/blob/main/src/SDEMP/TrinketUSBMIDIMultiPotMozziSynth3/TrinketUSBMIDIMultiPotMozziSynth3.ino
+    ^^ shows USB host mode
     
     Circuit with Adafruit Trinket M0
     * pin 1 (DAC 10b) audio output 
@@ -71,7 +72,8 @@ You can perform this tuning by either applying 3.0VDC to the VCO or finishing th
 #include <MozziGuts.h>
 #include <Oscil.h> // oscillator template
 // ~/Arduino/libraries/Mozzi/tables 
-#include <tables/cos2048_int8.h> // for the modulation oscillators
+# define TABLES_SIZE 2048
+// #include <tables/cos2048_int8.h> // for the modulation oscillators
 #include <tables/sin2048_int8.h> // sine table for oscillator
 #include <tables/saw2048_int8.h> // saw table for oscillator
 #include <tables/triangle2048_int8.h> // triangle table for oscillator
@@ -100,13 +102,13 @@ You can perform this tuning by either applying 3.0VDC to the VCO or finishing th
 
 
 // use #define for CONTROL_RATE, not a constant
-#define CONTROL_RATE 64  // Hz, powers of 2 are most reliable
+#define CONTROL_RATE 64  // Hz, powers of 2 are most reliable, lower numbers are "better" bc less load on the system
 #define CONTROL_SUBRATE 4  // how often you want to check controls that don't need to be checked everytime
 int control_cnt = 0;
 
 
 #define NUM_OSCILS 1
-Oscil<COS2048_NUM_CELLS, AUDIO_RATE> oscils[NUM_OSCILS];
+Oscil<TABLES_SIZE, AUDIO_RATE> oscils[NUM_OSCILS]; // is this template right? should it be bit width and am I missing updates anywhere?
 int oscils_note[NUM_OSCILS];
 int oscils_freq[NUM_OSCILS];
 bool oscils_playing[NUM_OSCILS];
@@ -162,7 +164,7 @@ bool accent_on = false;
 int accent_level = LVL_NORM;
 
 
-#define DEBUG 0
+#define DEBUG 1
 void debug_setup () {
   /* if debug off, we don't need serial */
   if (!DEBUG) {
@@ -285,30 +287,22 @@ y = (np.e**(np.log(255)/255))**y
 y.round(0)
  */
 const int lin_to_exp[256] = {
-         0,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,
-         1,   1,   1,   1,   1,   1,   1,   1,   2,   2,   2,
-         2,   2,   2,   2,   2,   2,   2,   2,   2,   2,   2,
-         2,   2,   2,   2,   2,   2,   2,   2,   2,   2,   3,
-         3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,
-         3,   3,   3,   4,   4,   4,   4,   4,   4,   4,   4,
-         4,   4,   4,   4,   5,   5,   5,   5,   5,   5,   5,
-         5,   5,   6,   6,   6,   6,   6,   6,   6,   6,   7,
-         7,   7,   7,   7,   7,   8,   8,   8,   8,   8,   8,
-         9,   9,   9,   9,   9,  10,  10,  10,  10,  10,  11,
-        11,  11,  11,  12,  12,  12,  12,  13,  13,  13,  14,
-        14,  14,  14,  15,  15,  15,  16,  16,  16,  17,  17,
-        18,  18,  18,  19,  19,  20,  20,  21,  21,  21,  22,
-        22,  23,  23,  24,  24,  25,  25,  26,  27,  27,  28,
-        28,  29,  30,  30,  31,  32,  32,  33,  34,  35,  35,
-        36,  37,  38,  39,  39,  40,  41,  42,  43,  44,  45,
-        46,  47,  48,  49,  50,  51,  52,  53,  55,  56,  57,
-        58,  59,  61,  62,  63,  65,  66,  68,  69,  71,  72,
-        74,  76,  77,  79,  81,  82,  84,  86,  88,  90,  92,
-        94,  96,  98, 100, 102, 105, 107, 109, 112, 114, 117,
-       119, 122, 124, 127, 130, 133, 136, 139, 142, 145, 148,
-       151, 155, 158, 162, 165, 169, 172, 176, 180, 184, 188,
-       192, 196, 201, 205, 210, 214, 219, 224, 229, 234, 239,
-       244, 250, 255
+         0,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,  //  16
+         1,   1,   1,   2,   2,   2,   2,   2,   2,   2,   2,   2,   2,   2,   2,   2,  //  32
+         2,   2,   2,   2,   2,   2,   2,   2,   2,   2,   2,   3,   3,   3,   3,   3,  //  48
+         3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   4,   4,   4,   4,   4,   4,  //  64
+         4,   4,   4,   4,   4,   4,   5,   5,   5,   5,   5,   5,   5,   5,   5,   6,  //  80
+         6,   6,   6,   6,   6,   6,   6,   7,   7,   7,   7,   7,   7,   8,   8,   8,  //  96
+         8,   8,   8,   9,   9,   9,   9,   9,  10,  10,  10,  10,  10,  11,  11,  11,  // 112
+        11,  12,  12,  12,  12,  13,  13,  13,  14,  14,  14,  14,  15,  15,  15,  16,  // 128
+        16,  16,  17,  17,  18,  18,  18,  19,  19,  20,  20,  21,  21,  21,  22,  22,  // 144
+        23,  23,  24,  24,  25,  25,  26,  27,  27,  28,  28,  29,  30,  30,  31,  32,  // 160  // [50] == 26 == 10% point
+        32,  33,  34,  35,  35,  36,  37,  38,  39,  39,  40,  41,  42,  43,  44,  45,  // 176
+        46,  47,  48,  49,  50,  51,  52,  53,  55,  56,  57,  58,  59,  61,  62,  63,  // 192
+        65,  66,  68,  69,  71,  72,  74,  76,  77,  79,  81,  82,  84,  86,  88,  90,  // 208
+        92,  94,  96,  98, 100, 102, 105, 107, 109, 112, 114, 117, 119, 122, 124, 127,  // 224
+       130, 133, 136, 139, 142, 145, 148, 151, 155, 158, 162, 165, 169, 172, 176, 180,  // 240
+       184, 188, 192, 196, 201, 205, 210, 214, 219, 224, 229, 234, 239, 244, 250, 255   // 256
 };
 
 
@@ -324,22 +318,25 @@ void updateControl () {
   int tbd_val = 0;
   int waveform = 0;
   int dcy_val = 0;
-  int dcy_ms = 200;
+  int dcy_ms = DCY_MIN;
   if (control_cnt > CONTROL_SUBRATE) {
     // these are controls that don't need to be responsive, reduce the I2C waits
     control_cnt = 0;
     tbd_val = adc_read(TBD_PIN);
     tbd_val = map(tbd_val, 0, 255, 0, 8);  
     tbd_val = constrain(tbd_val, 0, 7); 
+    byte notes[] = { 48, 50, 51, 53, 55, 56, 58, 60 };
     if (tbd_val != tbd) {
       // for testing, this is c harmonic minor
-      byte notes[] = { 48, 50, 51, 53, 55, 56, 58, 60 };
       HandleNoteOff(MIDI_CHANNEL, notes[tbd], LVL_NORM);
       // for testing alternate accent
       accent_on = !accent_on;
       if (DEBUG) { Serial.print("Tbd "); Serial.print(tbd); Serial.print(" -> "); Serial.print(tbd_val); Serial.print(" a? "); Serial.println(accent_on); } 
       tbd = tbd_val;
       HandleNoteOn(MIDI_CHANNEL, notes[tbd_val], LVL_NORM);
+    }
+    else if (venv[0].playing()) {
+      HandleNoteOff(MIDI_CHANNEL, notes[tbd], LVL_NORM);      
     }
     // freq = fn(note, tuning offset, glide, midi pitch bend)
     // wave = ratio of knob to number of options
@@ -353,8 +350,8 @@ void updateControl () {
     }
     // if !accent_on: fenv(atk=3 msec, dcy=200 msec +(2500-200)*ratio of knob)
     // if  accent_on: fenv(atk=3 msec, dcy=200 msec)
-    dcy_val = adc_read(DCY_PIN);
     if (!accent_on) {
+      dcy_val = adc_read(DCY_PIN);
       dcy_ms = map(dcy_val, 0, 255, DCY_MIN, DCY_MAX);
     }
     if (dcy_ms != decay) {
@@ -376,7 +373,9 @@ void updateControl () {
     // update_lpf = true;
   }
   // res = ratio of knob
-  //   res has an effect on fenv, the higher it is, the smoother the curve (C9 in schem)
+  //   if accent_on, res has an effect on fenv
+  //     the higher it is, the smoother the curve (more voltage from Accent knob availble to charge C13 in schem)
+  //     the lower it is, the accented decay cv is summed with the decay cv that went through the env mod
   //   on the 303 it is a double/stacked pot, 1 controls res, 2 compresses/smooths acc
   //   see https://www.firstpr.com.au/rwi/dfish/303-unique.html
   //   called the "Accent Sweep Circuit" and the smoother curve adds a wah/wow to the note
@@ -401,12 +400,11 @@ void updateControl () {
     env_mod = env;
     // update_lpf = true;
   }
-  fenv[0].update();
   // if !accent_on: cut = cut + fn(fenv(dcy=knob)*env_mod%)
   // if  accent_on: cut = cut + smooth_via_c13(res, fenv*acc%)
   //                res 0% is fenv/acc. res 100% smooth(fenv*acc%)
   //                see https://www.firstpr.com.au/rwi/dfish/303-unique.html
-  int cut_value = lin_to_exp[adc_read(CUT_PIN)];  // convert to exp (less change per step at lower values; more at higher)
+  int cut_value = lin_to_exp[adc_read(CUT_PIN)];  // convert to exp (less change per step at lower values; more at higher; for more intuitive knob turning)
   // int cut_freq = map(cut_value, 0, 255, CUT_MIN, CUT_MAX);
   // mozziAnalogRead value is 0-1023 AVR, 0-4095 on STM32; set with analogReadResolution in setup
   // note that this has been reduced to 8b until testing complete, so cut_value=0..255
@@ -415,20 +413,25 @@ void updateControl () {
     cutoff = cut_value;
     // update_lpf = true;
   }
+  // if anything is playing...
   // now we adjust the cut
   // for now, just give it the spike to know it works
-  int fenv_level = (fenv[0].next() * env_mod) >> 8;  // use env_mod as a % on fenv
+  fenv[0].update();  // does this need to happen every ctrl or can it be skipped if nothing is playing?
+  int fenv_level = (fenv[0].next() * env_mod) >> 8;  // use env_mod as a % on fenv and preserve 8b
   fenv_level = lin_to_exp[fenv_level];  // scale to be exponential decay
-  // find the headroom above the current cut, then use fenv as a % against that
-  fenv_level = ((CUT_MAX - cutoff) * fenv_level) >> 8;
+  // make a temp cut reduced by env_mod
+  // find the headroom above the temp cut, then use fenv as a % against that
+  fenv_level = ((CUT_MAX - cutoff) * fenv_level) >> 8;  // and preserve 8b
+  // avoid aliasing due to integer overflow, this is like hard clipping, could benefit from compression-like alg, or assurances against int overflow
   int tmp_cutoff = constrain(cutoff + fenv_level, 0, 255);
-  // if update_lpf {
+  // if update_lpf { and if nothing is playing then don't do the cutoff calcs
+  // might as well call this if anything changes, so there isn't the risk of a cut/res jump if a ctrl lags after a noteOn
   lpf.setCutoffFreqAndResonance(tmp_cutoff, resonance);
   // float venv_accent_boost = acc_pot_to_gain_val[acc];  // ret 1..(LVL_MAX/LVL_NORM)
   // venv(atk=3 msec, dcy=5000) * acc%*(LVL_MAX/LVL_NORM)
   // in audio: venv.next()*venv_accent_boost
   //           scale_8b_by_perc(venv.next(), venv_accent_boost)
-  venv[0].update();
+  venv[0].update();  // does this need to happen every ctrl or can it be skipped if nothing is playing?
 }
 
 
@@ -441,8 +444,55 @@ AudioOutput_t updateAudio () {
    *  for wave forms (like oscil, env shapes), call .update() per CONTROL_RATE in updateControl() and .next() here
    *    update calcs the actual value of that wave
    *    next extrapolates between actual and next
+   *  
+   * SAMD21 also in Feather M0
+   *  
+   *  updateAudio should return roughly 9b, with range of 488: -244 to 243
+   *  https://sensorium.github.io/Mozzi/doc/html/_mozzi_guts_8h_source.html
+   *  STANDARD_PLUS is default; nearly 9 bit sound (-244 to 243) at 16384 Hz sample rate (AUDIO_RATE)
+   *  However: https://sensorium.github.io/Mozzi/doc/html/_audio_config_s_a_m_d21_8h_source.html
+   *   and https://github.com/sensorium/Mozzi/issues/75
+   *   #define AUDIO_BITS 10 "make sure updateAudio returns values from -512 to 511
+   *   #define AUDIO_BIAS ((uint16_t) 1 << (AUDIO_BITS - 1)) aka 512
+   *     
+   *  venv[0].next returns 0..255, Q8n0 is same as unsigned char (uint8_t)
+   *   https://sensorium.github.io/Mozzi/doc/html/_a_d_s_r_8h_source.html#l00165
+   * 
+   *  osc.next reads the wave table, which is int8_t (signed 8b int) -128-127
+   *  https://sensorium.github.io/Mozzi/doc/html/_audio_output_8h_source.html ::46
+   *   #define AudioOutputStorage_t int
+   *  lpf reads osc multiplies feedback. also see note on line 154 about overflow https://sensorium.github.io/Mozzi/doc/html/_resonant_filter_8h_source.html
+   *    but that should only be by 1b: TODO calc max buf0/buf1 with f=1, fb=1, in={-128,127}
+   *    buf0 = buf0 + f * (in - buf0 + fb * (buf0 - buf1));
+   *    buf1 = buf1 + f * (buf0 - buf1)
+   *      127,127 = 127+(127-127) = 127
+   *      -128,-128 = -128+(-128+128)=-128
+   *      127,-128 = -128+(127+128)=127
+   *      -128,127 = 127+(-128-127)=-128
+   *       out = buf1; // LowPass
+   *       out = in - buf0; // HighPass
+   *       out = buf0 - buf1; // BandPass
+   *       out = in - buf0 + buf1; // Notch
    */
-  int vca_exp = lin_to_exp[venv[0].next()];  // 0..255 -> exp(0..255)
+  int32_t vca_exp = lin_to_exp[venv[0].next()];  // 0..255 -> exp(0..255)
   // TODO boost if accent_on
-  return MonoOutput::from8Bit((vca_exp * lpf.next(oscils[0].next())) >> 8);
+  // int8_t osc.next -128-127
+  // int9   lpf.next -256-255  // TODO verify
+  int32_t audio_out = lpf.next(oscils[0].next());  // AudioOutputStorage_t is an int
+  // trusting a comment in MultiResonantFilter example::86 to allow 1 bit for resonance
+  // filtered_osc should be <=9b
+  // now scale for vca, use x*y>>8 which will need 8+9=17b
+  audio_out = (vca_exp * audio_out) >> 8;
+  if (audio_out < -512) {  // (-(AudioOutputStorage_t) AUDIO_BIAS)
+    if (DEBUG) { Serial.print("Clipping - "); Serial.println(audio_out); }
+    audio_out = -512;
+  }
+  else if (audio_out > 511) {  // (AudioOutputStorage_t) AUDIO_BIAS-1
+    if (DEBUG) { Serial.print("Clipping + "); Serial.println(audio_out); }
+    audio_out = 511;
+  }
+  // For reasons, allow 1 bit of headroom to bring us to 10 bits, which is perfect for the SAMD21 DAC
+  // a bit of a hack, but let's clip this thing at 10b. see ::55 at https://sensorium.github.io/Mozzi/doc/html/_audio_output_8h_source.html
+  // #define CLIP_AUDIO(x) constrain((x), (-(AudioOutputStorage_t) AUDIO_BIAS), (AudioOutputStorage_t) AUDIO_BIAS-1)
+  return MonoOutput::fromNBit(10, audio_out);
 }
